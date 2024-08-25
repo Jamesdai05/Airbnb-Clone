9c9593336d4cc2ba3987b809e9fb262745580b1e
jamesdai05 <dai_guilong@hotmail.com>
2024-08-26 00:02:08 +0800

    added the user icon

--- my-react-app/src/Components/Header.jsx
+++ my-react-app/src/Components/Header.jsx
@@ -1,4 +1,8 @@
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import {
+  PaperAirplaneIcon,
+  Bars3Icon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
 const Header = () => {
   return (
@@ -31,7 +35,23 @@ const Header = () => {
             </svg>
           </button>
         </div>
-        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4"></div>
+        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+          <UserIcon />
+        </div>
       </header>
     </>
   );
