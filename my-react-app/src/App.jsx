f98a1aa48274d31af2b967c0ec3085ee6a44c8e6
jamesdai05 <dai_guilong@hotmail.com>
2024-08-25 17:47:30 +0800

    set up the vite with react and tailwind for the project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-6xl fond-bold underline text-center">
+      Hello vite React!
+    </h1>
+  );
+}
+
+export default App;
