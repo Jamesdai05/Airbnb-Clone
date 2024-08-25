e7ef889257043bab7a3bef55f5766e390d4bd957
jamesdai05 <dai_guilong@hotmail.com>
2024-08-26 00:03:30 +0800

    modified the size of burger

--- my-react-app/src/Components/Header.jsx
+++ my-react-app/src/Components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-8"
+            className="size-10"
           >
             <path
               strokeLinecap="round"
