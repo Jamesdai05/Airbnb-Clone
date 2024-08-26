f67e689fdf9ca89220443d026022a91fac27a9b6
jamesdai05 <dai_guilong@hotmail.com>
2024-08-26 13:27:37 +0800

    style the button in login page

--- my-react-app/src/pages/Login.jsx
+++ my-react-app/src/pages/Login.jsx
@@ -2,25 +2,36 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className="mt-8">
-      <h1 className="text-4xl mb-1 text-center">Login</h1>
-      <form action="" className="max-w-md mx-auto">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter your email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="password"
-        />
-        <button type="button">Login</button>
-      </form>
+    <div className="mt-1 grow flex items-center justify-around">
+      <div>
+        <h1 className="text-4xl mb-2 text-center">Login</h1>
+        <form action="" className="max-w-md mx-auto">
+          <label htmlFor="email" className="text-xl">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+          />
+          <label htmlFor="password" className="text-xl">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          <button
+            type="button"
+            className="w-full bg-primary p-2 text-white text-xl font-sans rounded-md mt-2"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
